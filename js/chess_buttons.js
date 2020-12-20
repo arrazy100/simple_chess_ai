885c8a4a8a9e9631d1190d151cf8fe4cc42f4018
afar0308 <afdhalarrazy111@gmail.com>
2020-12-20 07:07:27 +0700

    Perbaikan lebar game pada smartphone

--- js/chess_buttons.js
+++ js/chess_buttons.js
@@ -16,7 +16,7 @@ function playGame(level) {
     difficulty = level;
     document.getElementById("menu").style.display = "none";
     document.getElementById("game_screen").style.display = "inline-block";
-    if (detectMob()) $("#game_screen").width($(window).width());
+    if (detectMob()) $("#game_screen").width("100%");
     else $("#game_screen").width(500);
     board = Chessboard('myBoard', config);
     board.position(game.fen());
